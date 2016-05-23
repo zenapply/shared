774fc5b4a65f2172aaff20bc964b3c88dad38a3e
Tyler Arbon <tylercd100@gmail.com>
2016-05-23 15:59:58 -0600

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# shared-models
+# shared
+[![Latest Version](https://img.shields.io/github/release/zenapply/shared.svg?style=flat-square)](https://github.com/zenapply/shared/releases)
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square)](LICENSE.md)
+[![Build Status](https://travis-ci.org/zenapply/shared.svg?branch=master)](https://travis-ci.org/zenapply/shared)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/zenapply/shared/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/zenapply/shared/?branch=master)
+[![Code Coverage](https://scrutinizer-ci.com/g/zenapply/shared/badges/coverage.png?b=master)](https://scrutinizer-ci.com/g/zenapply/shared/?branch=master)
+[![Dependency Status](https://www.versioneye.com/user/projects/56f3252c35630e0029db0187/badge.svg?style=flat)](https://www.versioneye.com/user/projects/56f3252c35630e0029db0187)
+[![Total Downloads](https://img.shields.io/packagist/dt/zenapply/shared.svg?style=flat-square)](https://packagist.org/packages/zenapply/shared)
+
---
+++ src/Models/Base.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Zenapply\Shared\Models;
+
+use App\Exceptions\Model\ValidationException;
+use Config;
+use Log;
+use PulkitJalan\Cacheable\Cacheable;
+use Validator;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class Base extends Model
+{
+    use Cacheable {
+        boot as traitboot;
+    }
+
+    /**
+     * An array of validation rules
+     * @var array
+     */
+    protected $rules = [];
+
+    /**
+     * An array of validation messages
+     * @var array
+     */
+    protected $messages = [
+        'required_unless' => 'The :attribute field is required',
+        'required'        => 'The :attribute field is required',
+        'same'            => 'The :attribute and :other must match.',
+        'size'            => 'The :attribute must be exactly :size.',
+        'between'         => 'The :attribute must be between :min - :max.',
+        'in'              => 'The :attribute must be one of the following types: :values',
+    ];
+
+    /**
+     * Validates the Model
+     * @param  Model   $model The Model to check
+     * @return boolean        Whether or not the input is valid
+     */
+    public function validate(Model $model){
+        $validator = Validator::make($model->toArray(), $model->getValidationRules(), $model->getValidationMessages());
+        if($validator->fails()){
+            throw new ValidationException($validator->errors()->first());
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * A method best used to register Eloquent events when the model boots up
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        // PulkitJalan\Cacheable\Cacheable
+        self::traitboot();
+
+        self::saving(function($model){
+            if(!$model->validate($model)){
+                return false;
+            }
+        });
+    }
+
+    /**
+     * Returns an array of validation rules
+     * @return array An Array of validation rules
+     */
+    protected function getValidationRules(){
+        return $this->rules;
+    }
+
+    /**
+     * Returns an array of validation messages
+     * @return array An Array of validation messages
+     */
+    protected function getValidationMessages(){
+        return $this->messages;
+    }
+}
